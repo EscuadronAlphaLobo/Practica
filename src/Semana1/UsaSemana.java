e8e536f353fa925a32a1cf811cdd2625da295579
DAVID <DAVID@David>
2019-12-16 07:55:51 -0500

    Practica inicial a Github

---
+++ src/Semana1/UsaSemana.java
@@ -0,0 +1,10 @@
+
+package Semana1;
+
+public class UsaSemana {
+    public static void main(String[] args) {
+        Semana s=new Semana();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+    }
+}
